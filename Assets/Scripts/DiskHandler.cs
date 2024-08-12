14f250f569ec2effcf89984c71287434e9c52b34
Luka Uršič <luka.urs@icloud.com>
2024-08-12 22:32:12 +0200

    normal gameplayable, buggy reset
    - player can play normal game and win
    - by clicking a disk, there should be reset, but it moves them somewhere random...?

--- Assets/Scripts/DiskHandler.cs
+++ Assets/Scripts/DiskHandler.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DiskHandler : MonoBehaviour
@@ -7,73 +5,113 @@ public class DiskHandler : MonoBehaviour
     public bool isSelected = false;
     public Color originalColor;
     public Color selectedColor = Color.white;
+    public Color winColor = Color.yellow;
     private Renderer renderer;
+    private GameObject[] sticks;
+    private bool isGameWon = false;
+    private Vector3[] initialDiskPositions;
+    public float diskSize;
 
     private void Start()
     {
         renderer = GetComponent<Renderer>();
         originalColor = renderer.material.color;
+        sticks = GameObject.FindGameObjectsWithTag("Stick");
+
+        // initial disk positions
+        GameObject[] disks = GameObject.FindGameObjectsWithTag("Disk");
+        initialDiskPositions = new Vector3[disks.Length];
+        for (int i = 0; i < disks.Length; i++)
+        {
+            initialDiskPositions[i] = disks[i].transform.position;
+        }
     }
 
     private void OnMouseDown()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        if (isGameWon)
         {
-            if (hit.collider.gameObject.CompareTag("Disk"))
-            {
-                // Check for the topmost disk
-                GameObject topDisk = hit.collider.gameObject;
-                while (topDisk.transform.parent != null && topDisk.transform.parent.gameObject.CompareTag("Disk"))
-                {
-                    topDisk = topDisk.transform.parent.gameObject;
-                }
+            ResetGame();
+            return;
+        }
 
-                // Select the topmost disk
+        if (!isSelected)
+        {
+            // Check if clicked object is topmost
+            if (transform.parent == null || !transform.parent.CompareTag("Disk"))
+            {
                 isSelected = true;
                 renderer.material.color = selectedColor;
             }
         }
     }
 
-private void OnMouseUp()
-{
-    if (isSelected)
+    private void OnMouseUp()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        if (isSelected)
         {
-            if (hit.collider.gameObject.CompareTag("Stick"))
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                GameObject stick = hit.collider.gameObject;
-                if (CanPlaceDiskOnStick(stick))
+                if (hit.collider.gameObject.CompareTag("Stick"))
                 {
-                    // Snapping behavior
-                    float snapDistance = 0.1f;
-                    if (Vector3.Distance(transform.position, stick.transform.position) < snapDistance)
+                    GameObject stick = hit.collider.gameObject;
+                    if (CanPlaceDiskOnStick(stick))
                     {
-                        transform.position = stick.transform.position;
-                    }
+                        // Snapping behavior
+                        float snapDistance = 0.1f;
+                        if (Vector3.Distance(transform.position, stick.transform.position) < snapDistance)
+                        {
+                            transform.position = stick.transform.position;
+                        }
 
-                    transform.parent = stick.transform;
-                    transform.localPosition = new Vector3(0, transform.localPosition.y, 0);
+                        transform.parent = stick.transform;
+                        transform.localPosition = new Vector3(0, transform.localPosition.y, 0);
+
+                        CheckWinCondition();
+                    }
                 }
             }
+            isSelected = false;
+            renderer.material.color = originalColor;
         }
-        isSelected = false;
-        renderer.material.color = originalColor;
     }
-}
-
 
     private bool CanPlaceDiskOnStick(GameObject stick)
     {
         if (stick.transform.childCount == 0)
         {
-            return true; // Empty stick, can place disk
+            return true; // empty stick, can place disk
         }
 
-        Transform topDisk = stick.transform.GetChild(0);
-        return transform.position.y < topDisk.position.y;
+        Transform topDisk = stick.transform.GetChild(stick.transform.childCount - 1);
+        DiskHandler topDiskScript = topDisk.GetComponent<DiskHandler>();
+
+        return diskSize < topDiskScript.diskSize;
+    }
+
+    private void CheckWinCondition()
+    {
+        if (sticks[1].transform.childCount == 3)
+        {
+            isGameWon = true;
+            foreach (GameObject disk in GameObject.FindGameObjectsWithTag("Disk"))
+            {
+                disk.GetComponent<Renderer>().material.color = winColor;
+            }
+        }
+    }
+
+    private void ResetGame()
+    {
+        isGameWon = false;
+
+        GameObject[] disks = GameObject.FindGameObjectsWithTag("Disk");
+        for (int i = 0; i < disks.Length; i++)
+        {
+            disks[i].transform.position = initialDiskPositions[i];
+            disks[i].transform.parent = null;
+            disks[i].GetComponent<Renderer>().material.color = originalColor;
+        }
     }
 }
