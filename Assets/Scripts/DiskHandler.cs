83a0f9f735b4c1cd844e818176df56e0b7fb3336
urluur <luka.urs@icloud.com>
2024-08-12 16:38:52 +0200

    disks change color on drag
    - when disk is selected, it changes color to white, for better visual feedback and debugging
    - game logic doesn't want to work

--- Assets/Scripts/DiskHandler.cs
+++ Assets/Scripts/DiskHandler.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class DiskHandler : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public bool isSelected = false;
+    public Color originalColor;
+    public Color selectedColor = Color.white;
+    private Renderer renderer;
 
-    private bool isSelected = false;
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
 
     private void OnMouseDown()
     {
-        if (!isSelected)
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
         {
-            isSelected = true;
+            if (hit.collider.gameObject.CompareTag("Disk"))
+            {
+                // Check for the topmost disk
+                GameObject topDisk = hit.collider.gameObject;
+                while (topDisk.transform.parent != null && topDisk.transform.parent.gameObject.CompareTag("Disk"))
+                {
+                    topDisk = topDisk.transform.parent.gameObject;
+                }
+
+                // Select the topmost disk
+                isSelected = true;
+                renderer.material.color = selectedColor;
+            }
         }
     }
 
@@ -20,26 +40,34 @@ public class DiskHandler : MonoBehaviour
     {
         if (isSelected)
         {
-            isSelected = false;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                if (hit.collider.CompareTag("Stick"))
+                if (hit.collider.gameObject.CompareTag("Stick"))
                 {
-                    StartCoroutine(MoveToStick(hit.transform.position.x));
+                    if (CanPlaceDiskOnStick(hit.collider.gameObject))
+                    {
+                        GameObject stick = hit.collider.gameObject;
+                        transform.position = new Vector3(stick.transform.position.x, transform.position.y, stick.transform.position.z);
+                        transform.parent = stick.transform;
+                        transform.localPosition = new Vector3(0, transform.localPosition.y, 0);
+                    }
                 }
             }
+            isSelected = false;
+            renderer.material.color = originalColor;
         }
     }
 
-    private IEnumerator MoveToStick(float targetX)
+
+    private bool CanPlaceDiskOnStick(GameObject stick)
     {
-        while (transform.position.x != targetX)
+        if (stick.transform.childCount == 0)
         {
-            float step = moveSpeed * Time.deltaTime;
-            transform.position = new Vector3(Mathf.MoveTowards(transform.position.x, targetX, step), transform.position.y, transform.position.z);
-            yield return null;
+            return true; // Empty stick, can place disk
         }
+
+        Transform topDisk = stick.transform.GetChild(0);
+        return transform.position.y < topDisk.position.y;
     }
 }
